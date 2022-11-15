1bf016bd6bb8f3d4dd2c89c30d0b08bb37a694de
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-15 14:01:38 -0500

    using state to switch languages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import { useState } from 'react';
 import English from './lang/en.json';
 import Spanish from './lang/es.json';
+import './App.css';
 
 const local = navigator.language || 'en';
-const lang = (local === 'es') ? Spanish : English;
+const _lang = (local === 'es') ? Spanish : English;
 
 function App() {
+  const [lang, setLang] = useState(_lang);
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <button onClick={()=> setLang(English)}>EN</button>
+          <button onClick={()=> setLang(Spanish)}>ES</button>
+        </div>
         <h1>{lang.appHeader}</h1>
         <p>{lang.appSubhead}</p>
       </header>
